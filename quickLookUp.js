dd18cd34da6db36b55d3dcee6adfe1884c27714d
FranklinMiranda <fdm61344@gmail.com>
2022-03-23 18:05:19 -0400

    commit

--- quickLookUp.js
+++ quickLookUp.js
@@ -2,11 +2,11 @@
 console.log(new Date());
 
 // Strings
-strings();
-function strings() {
+stringsLookUp();
+function stringsLookUp() {
   const string = `string`;
   const string0 = "string";
-  const string1 = "string";
+  console.log(typeof string);
 
   console.log(string === string0);
   console.log(string.length);
@@ -35,4 +35,100 @@ function strings() {
   console.log(int);
   console.log(padSentence);
   console.log(padEnd);
+
+  const dictionary = "dictionary";
+  console.log(dictionary.indexOf("c"));
+  console.log(dictionary.lastIndexOf("i"));
+  console.log(dictionary.search("t"));
+  console.log(dictionary.match(/i/g));
+
+  console.log(dictionary.includes("i", 4));
+  console.log(dictionary.includes("i", 5));
+
+  console.log(dictionary.startsWith("d"));
+  console.log(dictionary.startsWith("D"));
+
+  console.log(dictionary.endsWith("ary"));
+  console.log(dictionary.endsWith("y"));
+
+  const temLit = 9;
+  console.log(`This is a template literal ${temLit}`);
 }
+
+numbersLookUp();
+function numbersLookUp() {
+  const num0 = 5;
+  const num1 = 12345.6789;
+  const num2 = Number.MAX_VALUE;
+  const num3 = Number.MIN_VALUE;
+  const string = "f";
+
+  console.log(typeof num0);
+  console.log(isNaN(string));
+
+  console.log(num1.toExponential());
+  console.log(num1.toExponential(2));
+  console.log(num1.toExponential(3));
+
+  console.log(num1.toFixed());
+  console.log(num1.toFixed(1));
+  console.log(num1.toFixed(2));
+
+  console.log(num1.toPrecision());
+  console.log(num1.toPrecision(1));
+  console.log(num1.toPrecision(2));
+
+  console.log(Number("6"));
+
+  console.log(parseInt(num1));
+  console.log(parseFloat(num1));
+  console.log(parseFloat(num0));
+
+  console.log(num2);
+  console.log(num3);
+}
+
+booleanLookUp();
+function booleanLookUp() {}
+
+arrayLookUp();
+function arrayLookUp() {
+  const cars = ["audi", "bmw", "ferrari", "honda", "toyota"];
+  const planets = ["mercury", "venus", "earth"]
+  console.log(cars);
+  console.log(cars[0]);
+  console.log(cars[cars.length - 1])
+  console.log(typeof cars);
+  console.log(cars.length)
+  console.log(Array.isArray(cars))
+  console.log(cars.toString())
+
+  cars.push("mercedes")
+  console.log(cars)
+  cars.pop()
+  console.log(cars)
+
+  cars.unshift("mercedes")
+  console.log(cars)
+  cars.shift()
+  console.log(cars)
+
+  console.log(cars.concat(planets))
+  const carplanet = cars.concat(planets)
+  
+  console.log(carplanet)
+  carplanet.splice(1, 2, "replace")
+  console.log(carplanet)
+
+  console.log(carplanet)
+  console.log(carplanet.slice(1, 4))
+
+  console.log(carplanet.includes("earth"))
+  
+}
+
+datesLookUp();
+function datesLookUp() {}
+
+MathLookUp();
+function MathLookUp() {}
