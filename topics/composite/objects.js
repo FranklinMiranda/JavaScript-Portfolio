b3b1d6727a5df396710b3b5ea00700eacc0a7113
FranklinMiranda <fdm61344@gmail.com>
2022-03-29 22:54:18 -0400

    commit

--- topics/composite/objects.js
+++ topics/composite/objects.js
@@ -1,2 +1,17 @@
 // Objects
-console.log(new Date())
+console.log(new Date());
+
+function objFactory(name, age) {
+  const obj = Object.create(baseObject);
+  obj.name = name;
+  obj.age = age;
+  return obj
+}
+
+const baseObject = {
+  greeting: function () {return `Hello ${this.name}, you are ${this.age} years old!`},
+};
+
+const person1 = objFactory('Franklin', 23)
+
+console.log(person1.greeting())
