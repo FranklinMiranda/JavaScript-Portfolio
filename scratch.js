115e3131e9cae91eca85f61901a4ba0aac6de819
FranklinMiranda <fdm61344@gmail.com>
2022-04-27 23:50:07 -0400

    com

--- scratch.js
+++ scratch.js
@@ -1,9 +1,25 @@
 // Scratch
 console.log(new Date());
 
-const name = 'franklAin'
+const name = 'franklAin';
 
-console.log(name.search(/a/))
+console.log(name.search(/a/));
 
-const name0 =name.replace(/a/ig,'f')
-console.log(name0)
+const name0 = name.replace(/a/gi, 'f');
+console.log(name0);
+
+function closureFunc(counter = 0) {
+  return function recursive(num = 0) {
+    if (counter > 10) {
+      console.log(counter)
+        return counter;
+    } else {
+      counter += num;
+      console.log(counter)
+      return recursive(num);
+    }
+  };
+}
+
+const closureFunc1 = closureFunc(1);
+console.log(closureFunc1(2))
