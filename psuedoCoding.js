7f9b32ec83052ada4b48ebc13064ff4e05113676
FranklinMiranda <fdm61344@gmail.com>
2023-01-11 12:48:21 -0500

    CM

---
+++ psuedoCoding.js
@@ -0,0 +1,6 @@
+// Title : PseudoCoding
+// Date : 1/11/2023
+// Author : Franklin Miranda
+// Description : PseudoCoding is used to descriptively explain coding solutions without writing executable code
+
+// Pseudocoding for Intersection Function that takes an array of arrays and returns an array of elements that are present in all the arrays
