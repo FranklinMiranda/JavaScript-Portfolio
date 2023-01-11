eb2cd58df4863b1c61f6a82db896dff894299639
FranklinMiranda <fdm61344@gmail.com>
2023-01-11 11:34:57 -0500

    CM

--- Event Driven Programming/callbackAsynchronous.js
+++ Event Driven Programming/callbackAsynchronous.js
@@ -5,12 +5,12 @@
 // setTimeout() and setInterval() are both methods of the HTML DOM Window object
 // clearTimeout() and clearInterval() are used to stop the executions of setTimeout() and setInterval() method respectively
 
-function message () {
-    console.log("This is a message")
+function message() {
+  console.log('This is a message');
 }
 
-setTimeout(message, 1000)
-clearTimeout(setTimeout(message, 1000))
+setTimeout(message, 1000);
+clearTimeout(setTimeout(message, 1000));
 
-setInterval(message, 1000)
-clearInterval(setInterval(message, 1000))
+setInterval(message, 1000);
+clearInterval(setInterval(message, 1000));
