3cab944ce67302745b8e61d14357df292b98309f
FranklinMiranda <fdm61344@gmail.com>
2023-01-11 11:23:28 -0500

    CM

--- Event Driven Programming/callbackAsynchronous.js
+++ Event Driven Programming/callbackAsynchronous.js
@@ -1,5 +1,4 @@
 // Title : Timing Events with Asynchronous Callbacks
 // Date : 1/11/2023
 // Author : Franklin Miranda
-// Description : setTimeout and setInterval can be used with the event loop to time and execute asynchronous operations
-
+// Description : setTimeout() and setInterval() can be used with the event loop to time and execute asynchronous operations
