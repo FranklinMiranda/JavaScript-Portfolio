e20978b6024a0fb8aaa3a965664345592b031162
FranklinMiranda <fdm61344@gmail.com>
2023-01-12 16:40:41 -0500

    CM

---
+++ Functional Programming/iife.js
@@ -0,0 +1,9 @@
+// Title : Immediately-invoked Function Expressions 
+// Date : 1/12/2023
+// Author : Franklin Miranda
+// Description : IIFEs are functions that are invoked when defined 
+
+(function iife (input) {
+    return console.log(input)
+}) ('Hello')
+
---
+++ PseudoCoding/pseudoCodingPractice2.js
@@ -0,0 +1,4 @@
+// Title : PseudoCoding 2
+// Date : 1/12/2023
+// Author : Franklin Miranda
+// Description : Pseudocode is an informal high-level description of a computer program or algorithm
