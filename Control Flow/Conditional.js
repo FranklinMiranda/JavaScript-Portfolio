5871bc88927d58c367bb797cd6164a14b8276468
FranklinMiranda <fdm61344@gmail.com>
2023-01-19 12:49:40 -0500

    CM

--- Control Flow/Conditional.js
+++ Control Flow/Conditional.js
@@ -2,3 +2,17 @@
 // Date : 1/16/2023
 // Author : Franklin Miranda
 // Description : Conditional Control Flow includes If, Else If, and Else Statements and Switch Statements
+
+// If, Else If, and Else Statements 
+const num = 10;
+const num1 = 20;
+
+if (num + num1 > 30) {
+  console.log(num + num1, 'greater than 30');
+} else if (num + num1 === 30) {
+  console.log(num + num1);
+} else {
+  console.log(num + num1, 'less than 30');
+}
+
+// Switch Statements 
--- Control Flow/Loops.js
+++ Control Flow/Loops.js
@@ -2,3 +2,4 @@
 // Date : 1/16/2023
 // Author : Franklin Miranda
 // Description : Loop Control Flow includes For, For in, For Of, While, and Do While Statements 
+
