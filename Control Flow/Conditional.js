57816512e088899e52a1b83c47ab800e82b93311
FranklinMiranda <fdm61344@gmail.com>
2023-01-19 12:59:52 -0500

    CM

--- Control Flow/Conditional.js
+++ Control Flow/Conditional.js
@@ -3,7 +3,7 @@
 // Author : Franklin Miranda
 // Description : Conditional Control Flow includes If, Else If, and Else Statements and Switch Statements
 
-// If, Else If, and Else Statements 
+// If, Else If, and Else Statements test for Equality and Logic, evaluating integers, characters, or boolean values
 const num = 10;
 const num1 = 20;
 
@@ -15,4 +15,31 @@ if (num + num1 > 30) {
   console.log(num + num1, 'less than 30');
 }
 
-// Switch Statements 
+// Switch Statements test for Equality, evaluating integers and characters
+// Switch statements use strict comparisions
+switch (new Date().getDay()) {
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+  default:
+    day = 'No day specified';
+}
+
+console.log(day);
